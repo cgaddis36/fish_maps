775aa6dd5d922b87454683ef4c6a842cd3065b20
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-12 10:38:26 -0600

    install bootstrap

--- spec/features/visitor/google_oauth_spec.rb
+++ spec/features/visitor/google_oauth_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe "As a visitor" do
   it "When I navigate to the homepage I can login with Google Oauth" do
     visit '/'
-    
+
     expect(page).to have_content("FishMaps")
 
-    expect(page).to have_button("Login with Google")
+    expect(page).to have_link("Login with Google")
 
     within "#about_me" do
-      expect(page).to have_content("Navigating Fishing Conservation Efforts")
+      expect(page).to have_content("Navigating Local Fishing Conservation Efforts")
     end
   end
 end
