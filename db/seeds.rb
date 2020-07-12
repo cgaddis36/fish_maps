0d5368e6987eaf8f7e5a4dad44f2c192f7cbaf3e
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-12 12:58:44 -0600

    seed creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+@user = User.create!(name: "Chase")
+@post1 = @user.posts.create!(title: "Rainbows on the 4th!", description: "15 in. Rainbow on a mysis Shrimp", city: "Silverthorne", state: "CO", body_of_water: "Blue River", region: 'Summit Co', photo: 'rainbow_blue_river.jpg')
+@rainbow = @post1.fish.create!(species: 'Rainbow Trout', harvested: false, length: 15, quantity: 1, photo: 'rainbow_blue_river.jpg')
