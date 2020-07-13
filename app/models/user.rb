6be421afad6e530f8eb267ad2ad4655aee9ea4b7
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-12 21:54:28 -0600

    add bcrypt to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,16 @@ class User < ApplicationRecord
   validates_presence_of :name
 
   has_many :posts
+  has_secure_password
+
 
   def self.update_or_create(auth_info)
     user = User.find_by(uid: auth_info[:uid]) || User.new
     user.attributes = { uid: auth_info[:uid],
                         email: auth_info[:info][:email],
                         name: auth_info[:info][:name],
-                        google_token: auth_info[:credentials][:token] }
+                        google_token: auth_info[:credentials][:token],
+                        password: "#{SecureRandom.hex(8)}" }
     user.save!
     user
   end
