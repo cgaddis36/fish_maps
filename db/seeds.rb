daff4d12011c945757a3426d91f556710153af7d
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-13 15:06:59 -0600

    seed config

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-@user = User.create!(name: "Chase", password_digest: "password_mania")
+@user = User.create!(name: "Chase", password: "password_mania")
 @post1 = @user.posts.create!(title: "Rainbows on the 4th!", description: "15 in. Rainbow on a mysis Shrimp", city: "Silverthorne", state: "CO", body_of_water: "Blue River", region: 'Central Colorado', fly_or_conventional: "Fly", privacy: "Public")
 @rainbow = @post1.fish.create!(species: 'Rainbow Trout', harvested: false, length: 15, quantity: 1, photo: 'rainbow_blue_river.jpg')
 @post2 = @user.posts.create!(title: "Dream Stream Cuttbow", description: "Cuttbow Wd-40 action", city: "Fairplay", state: "CO", body_of_water: "Dream Stream", region: 'South Park', fly_or_conventional: "Fly", privacy: "Private")
